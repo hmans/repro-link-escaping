0c35187396267f9e275b5f288cdc22ceb41f0ac2
Hendrik Mans <hendrik@mans.de>
2022-11-09 09:03:54 +0100

    minimal repro

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,12 @@
 import solid from "solid-start/vite";
 import { defineConfig } from "vite";
+import mdx from "solid-start-mdx";
+
 export default defineConfig({
-  plugins: [solid()],
+  plugins: [
+    await mdx(),
+    solid({
+      extensions: [".mdx", ".md"],
+    }),
+  ],
 });
